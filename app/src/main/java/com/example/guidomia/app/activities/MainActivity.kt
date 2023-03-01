e2d837981c081e26461e079581d4a9e7362b1aaf
a.elouazzani <RI52eiTG>
2023-03-01 13:10:33 +0100

    Init

--- app/src/main/java/com/example/guidomia/app/activities/MainActivity.kt
+++ app/src/main/java/com/example/guidomia/app/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.guidomia
+package com.example.guidomia.app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.guidomia.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
